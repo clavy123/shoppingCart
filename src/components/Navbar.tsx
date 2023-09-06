7d3372d9191fdf4e603f1db55542daf14a3d932e
Your Name <you@example.com>
2023-09-05 18:49:06 -0700

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {Container, Nav, Navbar as NavbarBs,Button } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useShoppingCart } from '../context/ShoppingCartContext';
+
+const Navbar = () => {
+  const{openCart,cartQuantity} = useShoppingCart();
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button style={{width:'3rem',height:'3rem',position:"relative"}} variant="outline-primary" className="rounded-circle" onClick={openCart}>
+          <AiOutlineShoppingCart/>
+          <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+              style={{
+                color: "white",
+                width: "1.5rem",
+                height: "1.5rem",
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                transform: "translate(25%, 25%)",
+              }}
+            >
+             {cartQuantity}
+            </div>
+        </Button>{' '}
+      </Container>
+    </NavbarBs>
+  )
+}
+
+export default Navbar
